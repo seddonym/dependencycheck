a774d8375e0e5c0027b13056aeb10f3d2c2f5ac7
David Seddon <david.seddon@octoenergy.com>
2018-06-12 13:40:03 +0100

    Initial tests for contract

--- dependencycheck/contract.py
+++ dependencycheck/contract.py
@@ -2,9 +2,23 @@ class ContractBroken(Exception):
     pass
 
 
+class Layer:
+    def __init__(self, name):
+        self.name = name
+
+
 class Contract:
+    def __init__(self, name, modules, layers, recursive=False):
+        self.name = name
+        self.modules = modules
+        self.layers = layers
+        self.recursive = recursive
+
     def check_dependencies(self, dependencies):
-        pass
+        path = dependencies.find_path(upstream=self.layers[1].name,
+                                      downstream=self.layers[0].name)
+        if path:
+            raise ContractBroken
 
 
 def get_contracts():
--- tests/unit/test_contract.py
+++ tests/unit/test_contract.py
@@ -1,2 +1,49 @@
+from unittest import mock
+import pytest
+from dependencycheck.contract import Contract, Layer, ContractBroken
+
+class TestGetContracts:
+    pass
+
+
 class TestContractCheck:
-    def test_nonrecursive_open_layers_
+    def test_simple_adherence_does_not_raise_exception(self):
+        contract = Contract(
+            name='Foo contract',
+            modules=(
+                'flowpackage',
+            ),
+            layers=(
+                Layer('upstream'),
+                Layer('downstream'),
+            ),
+        )
+        dep_graph = mock.Mock()
+        dep_graph.find_path.return_value = None
+
+        contract.check_dependencies(dep_graph)
+
+        dep_graph.find_path.assert_called_once_with(downstream='upstream',
+                                                    upstream='downstream')
+
+    def test_simple_break_raises_contract_broken(self):
+        contract = Contract(
+            name='Foo contract',
+            modules=(
+                'flowpackage',
+            ),
+            layers=(
+                Layer('upstream'),
+                Layer('downstream'),
+            ),
+        )
+        dep_graph = mock.Mock()
+        dep_graph.find_path.return_value = [
+            'downstream',
+        ]
+
+        with pytest.raises(ContractBroken):
+            contract.check_dependencies(dep_graph)
+
+        dep_graph.find_path.assert_called_once_with(downstream='upstream',
+                                                    upstream='downstream')
