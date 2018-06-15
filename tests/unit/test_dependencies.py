65e36ec424926dfbfc740a76997b14fb0d0f569b
David Seddon <david.seddon@octoenergy.com>
2018-06-15 10:46:41 +0100

    Add dependency unit tests

--- tests/unit/test_dependencies.py
+++ tests/unit/test_dependencies.py
@@ -1,24 +1,47 @@
+from unittest.mock import patch, Mock
+
 from dependencycheck.dependencies import DependencyGraph
 
 
 class TestDependencyGraph:
+    SOURCES = {
+        'one': Mock(
+            imports=[],
+        ),
+        'two': Mock(
+            imports=['one'],
+        ),
+        'three': Mock(
+            imports=['two'],
+        ),
+        'four': Mock(
+            imports=['three'],
+        ),
+    }
+
     def test_find_path_direct(self):
-        graph = DependencyGraph()
+        with patch.object(DependencyGraph, '_generate_pydep_sources',
+                          return_value=self.SOURCES):
+            graph = DependencyGraph('foo')
 
         path = graph.find_path(upstream='one', downstream='two')
 
         assert path == ('one',)
 
     def test_find_path_indirect(self):
-        graph = DependencyGraph()
+        with patch.object(DependencyGraph, '_generate_pydep_sources',
+                          return_value=self.SOURCES):
+            graph = DependencyGraph('foo')
 
         path = graph.find_path(upstream='one', downstream='four')
 
         assert path == ('three', 'two', 'one',)
 
     def test_find_path_nonexistent(self):
-        graph = DependencyGraph()
+        with patch.object(DependencyGraph, '_generate_pydep_sources',
+                          return_value=self.SOURCES):
+            graph = DependencyGraph('foo')
 
-        path = graph.find_path(upstream='one', downstream='four')
+        path = graph.find_path(upstream='four', downstream='one')
 
         assert path is None
